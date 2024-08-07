50ca8864601e683a06590313a72f28a8e97c5892
Deep Das <deepdblm@gmail.com>
2024-08-07 22:40:16 +0530

    made homepage responsive and final

--- homepage.js
+++ homepage.js
@@ -1,3 +1,39 @@
+var loader = document.querySelector("#loader")
+// set timeout basically dalay in execution
+setTimeout(function(){
+    loader.style.top = "-100%";
+},3500)
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const scrollContainer = document.getElementById('scrollContainer');
+
+
+//     const emptySpace = document.createElement('div');
+//     emptySpace.style.height = '2000px'; // Set this to the desired scrollable height
+//     scrollContainer.appendChild(emptySpace);
+
+
+//     document.body.style.overflow = 'auto';
+// });
+window.addEventListener('load', () => {
+    const gradientDiv = document.getElementById('gradientBackground');
+
+    // Example function to change gradient dynamically
+    function changeGradient(color1, color2) {
+        gradientDiv.style.background = `radial-gradient(${color1}, ${color2})`;
+    }
+
+    // Change the gradient after 3 seconds for demonstration
+    // setTimeout(() => {
+    //     changeGradient( "#000000","#000000");
+    // }, 3000);
+});
+
+
+
+
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
